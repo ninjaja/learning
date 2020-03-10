9bc356ba671d9b39e9e2c297c5860f028b86bee4
Dmitry Matrizaev <Dmitry.Matrizaev@itechart-group.com>
2020-03-10 12:33:47 +0300

    task 4: AutoBoxing refactored

--- src/task4Autoboxing/AutoBoxing.java
+++ src/task4Autoboxing/AutoBoxing.java
@@ -6,11 +6,11 @@ public class AutoBoxing {
 
         Integer a = new Integer(2);
         Integer a1 = new Integer(2);
-        int b = 2;
-        int b1 = 2;
+        int b = 127;
+        int b1 = 127;
         Integer c = 2;
-        int e = 128;
-        int e1 = 128;
+        Integer e = 127;
+        Integer e1 = 127;
 
         //reference comparison:
         System.out.println("a == a1 " + (a == a1));
@@ -20,10 +20,18 @@ public class AutoBoxing {
 
         //unboxing a, then compare base types:
         System.out.println("a == b " + (a == b));
+
+        //unboxing b, then compare base types:
         System.out.println("b == c " + (b == c));
 
-        //pool of integers capacity demo (-128..127)
-        System.out.println("b == b1 " + (b == b1));
-        System.out.println("e == e1 " + (e == 1));
+        //pool of Integers capacity demo (-128..127)
+        System.out.println("b == b1 " + (b == b1)); //true
+        b = b + c;
+        b1 = b1 + c;
+        System.out.println("b == b1 " + (b == b1)); //true, pool is not for base types
+        System.out.println("e == e1 " + (e == e1));
+        e = e + c;
+        e1 = e1 + c;
+        System.out.println("e == e1 " + (e == e1)); //false, out of pool range
     }
 }
