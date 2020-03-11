537b528331c783209e5bdda9de745148bb27ab40
Dmitry Matrizaev <Dmitry.Matrizaev@itechart-group.com>
2020-03-11 17:48:49 +0300

    task 6: annotations

---
+++ src/task6Annotation/AnnotationsTesting.java
@@ -0,0 +1,20 @@
+package task6Annotation;
+
+/**
+ * Demo of applying and accessing a custom annotation
+ */
+public class AnnotationsTesting {
+
+    public static void main(String[] args) throws Exception {
+        int times = AnnotationsTesting.class.getMethod("doSomething").getAnnotation(CustomAnnotation.class).value();
+        for (int i = 0; i < times; i++) {
+            doSomething();
+        }
+    }
+
+    @CustomAnnotation(3)
+    public static void doSomething() {
+        System.out.println("toss a coin");
+    }
+
+}
---
+++ src/task6Annotation/CustomAnnotation.java
@@ -0,0 +1,15 @@
+package task6Annotation;
+
+import java.lang.annotation.ElementType;
+import java.lang.annotation.Retention;
+import java.lang.annotation.RetentionPolicy;
+import java.lang.annotation.Target;
+
+/**
+ * Simple custom annotation that takes an integer as a {@code value}
+ */
+@Retention(RetentionPolicy.RUNTIME)
+@Target(ElementType.METHOD)
+public @interface CustomAnnotation {
+    int value();
+}
