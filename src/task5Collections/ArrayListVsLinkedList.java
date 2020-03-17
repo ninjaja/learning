af4957cf129578ae151941fdc99695cbfda3bff5
Dmitriy Matrizaev <dimmat8@gmail.com>
2020-03-17 17:29:13 +0300

    task5: TreeSetTesting and ArrayListVsLinkedList added

---
+++ src/task5Collections/ArrayListVsLinkedList.java
@@ -0,0 +1,86 @@
+package task5Collections;
+
+import java.util.ArrayList;
+import java.util.Iterator;
+import java.util.LinkedList;
+import java.util.List;
+
+/**
+ * Demo of {@code List} implementations performance
+ */
+public class ArrayListVsLinkedList {
+
+    private static final int AMOUNT = 200_000;
+    private static final int AMOUNT_BY_INDEX = 100_000;
+
+    private static List<Integer> arrayList = new ArrayList<>();
+    private static List<Integer> linkedList = new LinkedList<>();
+
+    public static void main(String[] args) {
+        addElements(arrayList);
+        addElements(linkedList);
+        addElementsByIndex(arrayList);
+        addElementsByIndex(linkedList);
+        getElements(arrayList);
+        getElements(linkedList);
+        removeElements(arrayList);
+        removeElements(linkedList);
+    }
+
+    private static void addElements(List<Integer> list) {
+        long startTime = System.currentTimeMillis();
+        for (int i = 0; i < AMOUNT; i++) {
+            list.add(i);
+        }
+        long endTime = System.currentTimeMillis();
+        float timeInSeconds = (endTime - startTime)/1000f;
+        System.out.println(list.getClass().getName() + " add " + AMOUNT + " elements time: " + timeInSeconds);
+
+    }
+
+    private static void addElementsByIndex(List<Integer> list) {
+        long startTime = System.currentTimeMillis();
+        for (int i = 0; i < AMOUNT_BY_INDEX; i++) {
+            list.add(i, i);
+        }
+        long endTime = System.currentTimeMillis();
+        float timeInSeconds = (endTime - startTime)/1000f;
+        System.out.println(list.getClass().getName() + " add " + AMOUNT_BY_INDEX + " elements by index time: " + timeInSeconds);
+
+    }
+
+    private static void getElements(List<Integer> list) {
+        long startTime = System.currentTimeMillis();
+        for (int i : list) {
+            list.get(i);
+        }
+        long endTime = System.currentTimeMillis();
+        float timeInSeconds = (endTime - startTime)/1000f;
+        System.out.println(list.getClass().getName() + " get elements time: " + timeInSeconds);
+    }
+
+    private static void removeElements(List<Integer> list) {
+        long startTime = System.currentTimeMillis();
+        Iterator<Integer> iterator = list.iterator();
+        while (iterator.hasNext()) {
+            iterator.next();
+            iterator.remove();
+        }
+        long endTime = System.currentTimeMillis();
+        float timeInSeconds = (endTime - startTime)/1000f;
+        System.out.println(list.getClass().getName() + " remove elements time: " + timeInSeconds);
+    }
+
+    /*
+    java.util.ArrayList add 200000 elements time: 0.011
+    java.util.LinkedList add 200000 elements time: 0.017
+    java.util.ArrayList add 100000 elements by index time: 8.736
+    java.util.LinkedList add 100000 elements by index time: 8.649
+    java.util.ArrayList get elements time: 0.013
+    java.util.LinkedList get elements time: 41.678
+    java.util.ArrayList remove elements time: 10.92
+    java.util.LinkedList remove elements time: 0.007
+    */
+
+
+}
---
+++ src/task5Collections/TreeSetTesting.java
@@ -0,0 +1,29 @@
+package task5Collections;
+
+import java.util.Collections;
+import java.util.Comparator;
+import java.util.Set;
+import java.util.TreeSet;
+
+/**
+ * Demo of  {@code TreeSet} with default sorting and custom {@code Comparator}
+ */
+public class TreeSetTesting {
+
+    public static void main(String[] args) {
+        Set<String> set = new TreeSet<>();
+        Set<String> customSet = new TreeSet<>(new Comparator<String>() {
+            @Override
+            public int compare(String o1, String o2) {
+                return o1.length() - o2.length();
+            }
+        });
+
+        String[] array = {"abcd", "bac", "cd", "fe", "e"};
+        Collections.addAll(set, array);
+        Collections.addAll(customSet, array);
+
+        assert ("[abcd, bac, cd, e, fe]").equals(set.toString());
+        assert ("[e, cd, bac, abcd]").equals(customSet.toString());
+    }
+}
